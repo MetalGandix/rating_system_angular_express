c5830e948a7017d9653223b7dc04bec3fd447860
Leonardo Mogianesi <leonardo.mogianesi@unicam.it>
2025-10-30 17:50:44 +0100

    Now it call back-end when it stars

--- Front-End/main.js
+++ Front-End/main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { fork } = require('child_process');
 
+let backendProcess;
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
@@ -12,13 +14,31 @@ function createWindow() {
     }
   });
 
-  win.loadFile(path.join(__dirname, 'dist/rating_system/index.html'));
+  // Carica l'app Angular compilata
+  win.loadFile(path.join(__dirname, 'dist', 'rating_system', 'index.html'));
+
+  // win.webContents.openDevTools(); // utile per debug
 }
 
 app.whenReady().then(() => {
+  try {
+    // Percorso dinamico del server (funziona ovunque)
+    const serverPath = path.join(__dirname, '..', 'app', 'server.js');
+    backendProcess = fork(serverPath);
+
+    backendProcess.on('error', err => console.error('Errore backend:', err));
+    backendProcess.on('exit', code => console.log(`Backend terminato con codice ${code}`));
+  } catch (err) {
+    console.error('Errore durante l\'avvio del backend:', err);
+  }
+
   createWindow();
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    // Chiude anche il backend quando chiudi l'app
+    if (backendProcess) backendProcess.kill();
+    app.quit();
+  }
 });
